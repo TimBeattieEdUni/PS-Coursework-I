9de823ca6cb7e61cb601251e7bb33de05be48c3a
Tim Beattie <s1471978@sms.ed.ac.uk>
2014-10-22 19:12:44 +0100

    Added the very basics: main, makefile, and gitignore.

---
+++ main.cpp
@@ -0,0 +1,22 @@
+//////////////////////////////////////////////////////////////////////////////
+/// @file       main.cpp
+///
+/// @brief      Provides main() for Programming Skills Group Practical.
+///
+
+
+//////////////////////////////////////////////////////////////////////////////
+//  Standard headers.
+#include <iostream>
+
+
+int main(int argc, char* argv[])
+{
+	(void) argc;
+	(void) argv;
+	
+	std::cout << "Hello, Group Practical!" << std::endl;
+	
+	return 0;
+}
+
