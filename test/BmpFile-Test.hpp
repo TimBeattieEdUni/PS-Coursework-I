31efdb4029bec7bfa2ede301cc84c8e747aa45af
David Canning <david.w.canning@gmail.com>
2014-11-06 17:47:45 +0000

    Changes to BmpFile.hpp and .dat to illustrate problem with BmpFile

--- test/BmpFile-Test.hpp
+++ test/BmpFile-Test.hpp
@@ -23,10 +23,22 @@ TEST(BmpFile)
 	using PsCourseworkI::BmpFile;
 
 	BmpFile bmp("inputs/BmpFile-Test.dat");
-	CHECK(bmp.GetArray()(0,0) == 1);
+
+   for ( int i=0; i<2; i++)
+   {
+      for (int j=0; j<3; j++)
+      {
+         std::cout << "bmp.GetArray(" <<i << "," << j << ") = " << bmp.GetArray()(i,j) << std::endl; 
+      }
+   }
+
+   CHECK(bmp.GetArray()(0,0) == 1);
 	CHECK(bmp.GetArray()(1,0) == 23);
 	CHECK(bmp.GetArray()(0,1) == 0);
 	CHECK(bmp.GetArray()(1,1) == 4);
+	
+   CHECK(bmp.GetArray()(0,2) == 5);
+	CHECK(bmp.GetArray()(1,2) == 6);
 }
 
 
