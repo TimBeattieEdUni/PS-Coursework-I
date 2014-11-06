690dd7ffbc3d6faa0ca69e6c8721959174443395
Max Sorantin <s1474365@sms.ed.ac.uk>
2014-11-06 15:04:08 +0000

    added matlab plots for the profiling, still needs to export them

---
+++ scratch/Timings.m
@@ -0,0 +1,45 @@
+clear all
+clc
+
+
+load('Timings.txt');
+
+lenghts=Timings(:,1);
+
+O3_0=Timings(:,2);
+O3_1=Timings(:,3);
+O3_2=Timings(:,4);
+O4=Timings(:,5);
+O5=Timings(:,6);
+
+DO4=(-O4./O3_2+1)*100;
+DO5=(-O5./O3_2+1)*100;
+
+
+[f,gof]=fit(lenghts,O5,'poly2');
+
+nlf=NonLinearModel.fit(lenghts, O5,@(b,x) b(1)+b(2)*x.^2, [0,1])
+
+q=@(b,x) b(1)+b(2)*x.^2;
+
+A=nlinfit(lenghts, O5,q, [0,1])
+
+yfit=q(A,lenghts);
+
+figure(1)
+xlabel('Grid size')
+ylabel('time [s]')
+plot(lenghts, O3_0,'+-', lenghts, O3_1,'x-',lenghts, O3_2,'o-' )
+
+
+figure(2)
+xlabel('Grid size')
+ylabel('time [s]')
+plot(lenghts, O3_2,'+-', lenghts, O4,'x-',lenghts, O5,'o-')
+
+figure(3)
+bar(lenghts,[DO4,DO5])
+
+figure(4)
+plot(lenghts, O5,'r x',lenghts,yfit)
+
