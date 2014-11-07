5c92b85ee922e88c4bd5092ca7b0c91074a76417
Max Sorantin <s1474365@sms.ed.ac.uk>
2014-11-07 10:21:59 +0000

    Timing class implemented

---
+++ classes/Timing.cpp
@@ -0,0 +1,66 @@
+//////////////////////////////////////////////////////////////////////////////
+/// @file     Timing.cpp
+///
+/// @brief    Implementation of class Timing.
+///
+
+
+//////////////////////////////////////////////////////////////////////////////
+//  This class's header.
+#include "Timing.hpp"
+
+
+
+namespace PsCourseworkI
+{
+	//////////////////////////////////////////////////////////////////////////////
+	/// @details    Stores the elapsed time.
+	///
+	///
+	Timing::Timing()
+	: m_start(), m_end()
+	{
+	}
+
+    //////////////////////////////////////////////////////////////////////////////
+	/// @details    Starts the timing
+	///
+	///
+	void Timing::ResetTiming()
+	{
+	    //set starting time
+        gettimeofday(&m_start,NULL); // on windows OS use: GetSystemTime
+
+	}
+
+
+    //////////////////////////////////////////////////////////////////////////////
+	/// @details    Rports the time difference since last reset
+	///
+	///
+	void Timing::ReportTiming(
+                            unsigned int &minutes,
+                            unsigned int &seconds,
+                            unsigned int &milliseconds
+                            )
+	{
+        // timing:
+        gettimeofday(&m_end,NULL);
+
+        unsigned int seconds_diff=(m_end.tv_sec - m_start.tv_sec);
+        unsigned int microseconds_diff=(m_end.tv_usec - m_start.tv_usec);
+
+        //  transforming the seconds and microseconds output in usaual format (h,min,sec,msec,usec)
+
+        //uncommend if you want hours
+        //hours=(seconds_diff/3600);
+
+        minutes = seconds_diff / 60;
+        seconds = seconds_diff - (hours * 3600) - (minutes * 60);
+        milliseconds = microseconds_diff / 1000;
+
+
+	}
+
+
+}   //  namespace PsCourseworkI
---
+++ classes/Timing.hpp
@@ -0,0 +1,50 @@
+//////////////////////////////////////////////////////////////////////////////
+/// @file     Timing.hpp
+///
+/// @brief    Interface declaration for class Timing.
+///
+
+
+#ifndef PSCOURSEWORKI_TIMING_HPP
+#define PSCOURSEWORKI_TIMING_HPP
+
+
+//////////////////////////////////////////////////////////////////////////////
+//  Local headers.
+
+
+
+//////////////////////////////////////////////////////////////////////////////
+//  Standard headers.
+#include <iostream>
+#include <ctime>
+#include <sys/time.h>
+
+
+namespace PsCourseworkI
+{
+	//////////////////////////////////////////////////////////////////////////////
+	/// @brief      Stopper watch: class for timing
+	///
+	/// @details    Start, finish and report elapsed time
+	///
+	class Timing
+	{
+		public:
+            Timing();                                        ///< Constructor.
+
+            void ResetTiming();                              ///< Setter
+            void ReportTiming(unsigned int &hours,
+                              unsigned int &minutes,
+                              unsigned int &seconds,
+                              unsigned int &milliseconds);  /// Getter
+
+		private:
+            timeval m_start,m_end;                          ///< Time stamps
+
+	};
+
+}   //  namespace PsCourseworkI
+
+
+#endif  //  #ndef PSCOURSEWORKI_TIMING_HPP
